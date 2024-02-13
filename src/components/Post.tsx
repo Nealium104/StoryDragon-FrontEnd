23915a3787846d238072dbd82f60d8f24164734e
Neal Grindstaff <neal.grindstaff@gmail.com>
2024-02-12 21:28:39 -0500

    added more to post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,13 +3,20 @@ import { FaBook, FaPerson, FaGlobe } from "react-icons/fa6";
 export default function Post({ post }) {
   return (
     <article className="bg-background-secondary">
-      {post.PostType == "Story" ? (
-        <FaBook />
-      ) : post.PostType == "Location" ? (
-        <FaGlobe />
-      ) : (
-        <FaPerson />
-      )}
+      <div className="flex">
+        <div className="w-12 h-12 m-4">
+          {post.PostType == "Story" ? (
+            <FaBook className="w-full h-full" />
+          ) : post.PostType == "Location" ? (
+            <FaGlobe className="w-full h-full" />
+          ) : (
+            <FaPerson className="w-full h-full" />
+          )}
+        </div>
+        <div>
+          <div className="w-24 h-24 m-4 rounded-full bg-red-500"></div>
+        </div>
+      </div>
       <p className="">{post.PostText}</p>
       <p>This is also a div</p>
     </article>
